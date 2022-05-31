22e9f58ddb1e10728e881304bb64806564ce2e00
manachat <vafilonov@edu.hse.ru>
2022-05-31 12:10:56 +0300

    csv

--- hadoop-rasters/src/main/java/vafilonov/hadooprasters/core/processing/stage/context/CSVExtractionOutputContext.java
+++ hadoop-rasters/src/main/java/vafilonov/hadooprasters/core/processing/stage/context/CSVExtractionOutputContext.java
@@ -4,9 +4,20 @@ import javax.annotation.Nullable;
 
 import org.apache.hadoop.conf.Configuration;
 import vafilonov.hadooprasters.core.processing.stage.hadoop.HadoopStageContext;
+import vafilonov.hadooprasters.core.processing.stage.hadoop.HadoopStageContextCarcass;
 import vafilonov.hadooprasters.core.processing.stage.hadoop.StageResource;
 
-public class CSVExtractionOutputContext implements HadoopStageContext {
+public class CSVExtractionOutputContext extends HadoopStageContextCarcass {
+
+    private String outDir;
+    private final boolean success;
+
+    public CSVExtractionOutputContext(Configuration conf, String out, boolean success) {
+        super(conf);
+        outDir = out;
+        this.success = success;
+    }
+
     @Override
     public boolean isSuccessFull() {
         return false;
@@ -25,11 +36,6 @@ public class CSVExtractionOutputContext implements HadoopStageContext {
 
     @Override
     public StageResource.CacheStageResource getCacheStageResources() {
-        return null;
-    }
-
-    @Override
-    public Configuration getConfiguration() {
         return null;
     }
 }
